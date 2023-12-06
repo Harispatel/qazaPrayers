9f8f4cb1d037fcc9cfe6c98d0b938015bf03fc61
unknown <harispatel62@gmail.com>
2023-12-06 23:03:48 +0300

    Initial project setup

---
+++ pages/Login.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable prettier/prettier */
+import React, {useState} from 'react';
+import {View, TextInput, Button, StyleSheet} from 'react-native';
+
+const LoginScreen: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Authentication logic
+    console.warn('Logging in with:', {username, password}),
+    console.log('Logging in with:', {username, password});
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={text => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={text => setPassword(text)}
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+});
+
+export default LoginScreen;
