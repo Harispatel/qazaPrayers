71db13850ff40b68cb3dc1e03e50c14f543335dd
zaheer <zaheer.mantiqh.com>
2023-12-12 23:58:31 +0530

    sign up page

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,4 @@
+import apple from './app-store.png';
+import google from './google.png';
+
+export {apple, google};
--- src/components/common/styles.js
+++ src/components/common/styles.js
@@ -20,29 +20,29 @@ export default StyleSheet.create({
   },
 
   content: Object.assign({}, Typography.bodyText),
-  nextBtn:{
-    padding:15,
-    paddingLeft:25,
-    borderBottomStartRadius:30,
-    borderTopStartRadius:30,
+  nextBtn: {
+    padding: 15,
+    paddingLeft: 25,
+    borderBottomStartRadius: 30,
+    borderTopStartRadius: 30,
   },
-  skipBtn:{
-    marginRight:width-150,
-    padding:15,
-    paddingLeft:25,
-    color:COLORS.black.absolute,
+  skipBtn: {
+    marginRight: width - 150,
+    padding: 15,
+    paddingLeft: 25,
+    color: COLORS.black.absolute,
   },
-  prevBtn:{
-    width:60,
-    borderRadius:50,
-    padding:5,
+  prevBtn: {
+    width: 60,
+    borderRadius: 50,
+    padding: 5,
   },
   // Walkthrough Styling
-  walkthroughContainer:{
+  walkthroughContainer: {
     flex: 1,
     flexDirection: 'row',
     width: '100%',
-    paddingBottom:20,
+    paddingBottom: 20,
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
   },
@@ -54,25 +54,82 @@ export default StyleSheet.create({
   },
   prvTxt: {
     color: COLORS.green.default,
-    transform: [{ rotate: '180deg'}],
-    marginTop:10,
-    fontSize:30,
-    textAlign:'center'
+    transform: [{rotate: '180deg'}],
+    marginTop: 10,
+    fontSize: 30,
+    textAlign: 'center',
   },
-  
+
   logo: {
     width: width,
     height: 250,
   },
-  description:{
-    textAlign:'center'
+  description: {
+    textAlign: 'center',
   },
   // Common Styling
-  rowDirection:{
-    flexDirection: 'row'
+  rowDirection: {
+    flexDirection: 'row',
   },
-  viewFlex:{
-    flex: 1
-  }
-
+  viewFlex: {
+    flex: 1,
+  },
+  textBlack: {
+    color: 'black',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'white',
+    width: '100%',
+    height: 60,
+    backgroundColor: '#CDCDCD',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    fontSize: 18,
+  },
+  buttonDark: {
+    backgroundColor: '#4D4D4D',
+    width: '100%',
+    height: 60,
+    borderRadius: 50,
+  },
+  buttonDarkText: {
+    textAlign: 'center',
+    marginVertical: 15,
+    color: 'white',
+  },
+  semiBold: {
+    fontWeight: '800',
+  },
+  textLG: {
+    fontSize: 20,
+  },
+  textMD: {
+    fontSize: 15,
+  },
+  mt45: {
+    marginVertical: 45,
+  },
+  mt15: {
+    marginVertical: 15,
+  },
+  signUpIcon: {
+    width: 60,
+    height: 60,
+  },
+  flex: {
+    flexDirection: 'row',
+  },
+  justifyContentEvenly: {
+    justifyContent: 'space-evenly',
+  },
+  justifyContentCenter: {
+    justifyContent: 'center',
+  },
+  gap4: {
+    gap: 30,
+  },
+  gap1: {
+    gap: 5,
+  },
 });
